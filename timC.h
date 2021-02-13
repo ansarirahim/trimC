f6d968745632d37bc5fda3074a473f693353c319
AbdulRaheem <ansarirahim1@gmail.com>
2021-02-14 02:36:04 +0530

    Create timC.h
    header file

---
+++ timC.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char *ltrim(char *s);
+
+char *rtrim(char *s);
+
+char *trim(char *s);
